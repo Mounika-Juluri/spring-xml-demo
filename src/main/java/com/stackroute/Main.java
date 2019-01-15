24d23741be60207e60356fb4c43762e4c65c5c5d
Mounika <julurimounika96@gmail.com>
2019-01-15 18:32:34 +0530

    constructor based injection

--- src/main/java/com/stackroute/Main.java
+++ src/main/java/com/stackroute/Main.java
@@ -15,18 +15,26 @@ public class Main {
     public static void main(String[] args){
 
         BeanFactory beanFactory=new XmlBeanFactory(new ClassPathResource("beans.xml"));
-        Movie movie=(Movie) beanFactory.getBean("movie");
+        Movie movie=(Movie) beanFactory.getBean("MovieA");
         System.out.println(movie.getActor());
+        Movie movie6=(Movie) beanFactory.getBean("MovieB");
+        System.out.println(movie6.getActor());
+        System.out.println(movie==movie6);
 
         BeanDefinitionRegistry beanDefinitionRegistry=new DefaultListableBeanFactory();
         BeanDefinitionReader beanDefinitionReader=new XmlBeanDefinitionReader(beanDefinitionRegistry);
         beanDefinitionReader.loadBeanDefinitions(new ClassPathResource("beans.xml"));
-        Movie movie2=((DefaultListableBeanFactory) beanDefinitionRegistry).getBean(Movie.class);
+        Movie movie2=(Movie)((DefaultListableBeanFactory) beanDefinitionRegistry).getBean("movie1");
         System.out.println(movie2.getActor());
 
         ApplicationContext context=new ClassPathXmlApplicationContext("beans.xml");
-        Movie movie3=context.getBean(Movie.class);
+        Movie movie3=(Movie)context.getBean("movie1");
         System.out.println(movie3.getActor());
+
+        Movie movie4=(Movie)context.getBean("movie2");
+        System.out.println(movie4.getActor());
+
+        System.out.println(movie3==movie4);
 
     }
 }
